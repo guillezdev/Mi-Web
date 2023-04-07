42dd821c47e2d422197003f626ecbaa2bf93155e
guilledev <guillermoprogcc@gmail.com>
2023-04-06 22:25:13 -0400

    Agregando Funcionalidad al Contact-Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 
 function App() {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaDiscord, FaWhatsapp } from 'react-icons/fa';
+import { FaGithub, FaWhatsapp } from 'react-icons/fa';
 
 function Footer() {
   return (
