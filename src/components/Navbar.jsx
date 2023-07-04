0a2cd3ddac944c88503e30f2a7f39f4028af0c72
guillezdev <guillermoprogcc@gmail.com>
2023-07-03 22:14:53 -0400

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,8 @@ import { Transition } from "@headlessui/react";
 import { Link } from "react-scroll";
 import Favicon from "../assets/logo.svg";
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.scrollY;
-      setIsScrolled(position > 0);
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -34,73 +20,72 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='h-24 w-full'></div>
+      <div className="h-24 w-full"></div>
       <nav
-        className={`fixed py-0 top-0 left-0 right-0 z-50 bg-white  backdrop-filter bg-opacity-10 shadow-sm backdrop-blur-sm 
-      ${isScrolled ? "" : ""}`}
+        className={`fixed py-0 top-0 left-0 right-0 z-50 bg-white  backdrop-filter bg-opacity-10 shadow-sm backdrop-blur-sm`}
       >
-        <div className='container mx-auto py-4 px-6 md:px-8'>
-          <div className='flex justify-between items-center'>
+        <div className="container mx-auto py-4 px-6 md:px-8">
+          <div className="flex justify-between items-center">
             <Link
-              to='home'
+              to="home"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              cursor='pointer'
-              className='text-xl font-bold text-gray-900 cursor-pointer select-none hover:scale-90'
+              cursor="pointer"
+              className="text-xl font-bold text-gray-900 cursor-pointer select-none hover:scale-90"
             >
-              <img src={Favicon} className='h-10 m-0 p-0' alt='GZD' />
+              <img src={Favicon} className="h-10 m-0 p-0" alt="GZD" />
             </Link>
-            <div className='hidden md:block'>
-              <ul className='flex space-x-4'>
+            <div className="hidden md:block">
+              <ul className="flex space-x-4">
                 <li>
                   <Link
-                    to='home'
+                    to="home"
                     spy={true}
                     smooth={true}
                     offset={-70}
                     duration={500}
-                    className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                    className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                   >
                     Inicio
                   </Link>
                 </li>
                 <li>
                   <Link
-                    to='projects'
+                    to="projects"
                     spy={true}
                     smooth={true}
                     offset={-70}
                     duration={500}
-                    cursor='pointer'
-                    className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                    cursor="pointer"
+                    className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                   >
                     Proyectos
                   </Link>
                 </li>
                 <li>
                   <Link
-                    to='skills'
+                    to="skills"
                     spy={true}
                     smooth={true}
                     offset={-70}
                     duration={500}
-                    cursor='pointer'
-                    className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                    cursor="pointer"
+                    className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                   >
                     Habilidades
                   </Link>
                 </li>
                 <li>
                   <Link
-                    to='contact'
+                    to="contact"
                     spy={true}
                     smooth={true}
                     offset={-70}
                     duration={500}
-                    cursor='pointer'
-                    className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                    cursor="pointer"
+                    className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                   >
                     Contacto
                   </Link>
@@ -108,41 +93,41 @@ const Navbar = () => {
               </ul>
             </div>
 
-            <div className='md:hidden'>
+            <div className="md:hidden">
               <button
-                className='flex items-center justify-center h-8 w-8 text-gray-900 hover:shadow-xl py-1 px-2 rounded-md focus:outline-none'
+                className="flex items-center justify-center h-8 w-8 text-gray-900 hover:shadow-xl py-1 px-2 rounded-md focus:outline-none"
                 onClick={() => setIsOpen(!isOpen)}
               >
                 {isOpen ? (
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                    aria-hidden='true'
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
                   >
                     <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M6 18L18 6M6 6l12 12'
+                      d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
                 ) : (
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                    aria-hidden='true'
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
                   >
                     <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M4 6h16M4 12h16M4 18h16'
+                      d="M4 6h16M4 12h16M4 18h16"
                     />
                   </svg>
                 )}
@@ -152,24 +137,24 @@ const Navbar = () => {
 
           <Transition
             show={isOpen}
-            enter='transition ease-out duration-100 transform'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='transition ease-in duration-75 transform'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
             {
-              <div className=' md:hidden mt-4'>
-                <ul className='flex flex-col items-center space-y-2'>
+              <div className=" md:hidden mt-4">
+                <ul className="flex flex-col items-center space-y-2">
                   <li>
                     <Link
-                      to='home'
+                      to="home"
                       spy={true}
                       smooth={true}
                       offset={-70}
                       duration={500}
-                      className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                      className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                       onClick={() => setIsOpen(!isOpen)}
                     >
                       Inicio
@@ -177,13 +162,13 @@ const Navbar = () => {
                   </li>
                   <li>
                     <Link
-                      to='projects'
+                      to="projects"
                       spy={true}
                       smooth={true}
                       offset={-70}
                       duration={500}
-                      cursor='pointer'
-                      className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                      cursor="pointer"
+                      className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                       onClick={() => setIsOpen(!isOpen)}
                     >
                       Proyectos
@@ -191,13 +176,13 @@ const Navbar = () => {
                   </li>
                   <li>
                     <Link
-                      to='skills'
+                      to="skills"
                       spy={true}
                       smooth={true}
                       offset={-70}
                       duration={500}
-                      cursor='pointer'
-                      className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                      cursor="pointer"
+                      className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                       onClick={() => setIsOpen(!isOpen)}
                     >
                       Habilidades
@@ -205,13 +190,13 @@ const Navbar = () => {
                   </li>
                   <li>
                     <Link
-                      to='contact'
+                      to="contact"
                       spy={true}
                       smooth={true}
                       offset={-70}
                       duration={500}
-                      cursor='pointer'
-                      className='text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none'
+                      cursor="pointer"
+                      className="text-gray-900 hover:shadow-xl py-1 px-2 rounded-md font-medium cursor-pointer select-none"
                       onClick={() => setIsOpen(!isOpen)}
                     >
                       Contacto
