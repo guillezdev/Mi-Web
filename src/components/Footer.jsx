9433936a09c3646883705f04aac59fb84434f897
guillezdev <guillezdev@gmail.com>
2023-08-01 19:19:16 -0400

    some changes in styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="bg-gray-800 text-white py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-gray-400 text-center">
-          Copyright © 2023 Creado por Guillezdev.
+         {"Sin Copyright Creado con React.js por Guillezdev."}
         </p>
       </div>
     </footer>
